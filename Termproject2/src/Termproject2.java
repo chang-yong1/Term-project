1e7d022c28e71ea1a85dfd072d322e7fa584ede8
changyong Choi <gkxmrkqyd19@naver.com>
2020-11-27 04:12:38 +0900

    시간추가와 코드수정

--- Termproject2/src/Termproject2.java
+++ Termproject2/src/Termproject2.java
@@ -11,16 +11,17 @@ public class Termproject2 {
 		
 		System.out.println("생성할 데이터의 개수를 입력해주세요");
 		int data_n=sc.nextInt();
+	
+		System.out.println("렌덤으로 생성할 구간의 개수를 입력해주세요");
+		int range_n=sc.nextInt();
+		long start = System.currentTimeMillis();
+		int sum =0;
 		int arr[]=new int[data_n];
 		for(int i=0;i<data_n;i++) {
 			arr[i]=rn.nextInt();//n개의 데이터 셍성
 		}
-		System.out.println("렌덤으로 생성할 구간의 개수를 입력해주세요");
-		int range_n=sc.nextInt();
-		
 		int min = arr[a];
 		int max = arr[a];
-		int sum =0;
 		
 		for(int j=0;j<range_n;j++) {
 			while(true) {
@@ -28,7 +29,7 @@ public class Termproject2 {
 				b=rn.nextInt(data_n);//구간 끝 점
 				if(a<=b) break;
 			}
-			System.out.println(a+"  "+b);
+			//System.out.println(a+"  "+b);
 		
 			for(int k=a;k<=b;k++) {
 				if(min>=arr[k]) min=arr[k];
@@ -38,8 +39,10 @@ public class Termproject2 {
 			System.out.println("최솟값: "+min);
 			System.out.println("최댓값: "+max);
 			System.out.println("합계 :"+sum);
-
+			//11월22일
 		}
+		long end = System.currentTimeMillis();
+		System.out.println("걸린시간은: "+(end-start)+"ms입니다" );
 	}
 
 }
